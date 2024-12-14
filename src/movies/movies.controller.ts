45256e4dcb4e9aa4451cbc8585de8d6fa22ccb55
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-12-14 16:01:37 +0900

    [#2.2] controller 기능 실습2

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,21 +15,31 @@ export class MoviesController {
   getAll() {
     return 'all movies';
   }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `searching title ${searchingYear}`;
+  }
+
   @Get('/:id/')
   getOne(@Param('id') movieId: string) {
     return 'one movie ' + movieId;
   }
 
   @Post()
-  create() {
-    return 'create movie';
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
   @Delete('/:id/')
   remove(@Param('id') movieId: string) {
     return `delete ${movieId} movie`;
   }
   @Patch('/:id/')
-  patch(@Param('id') movieId: string) {
-    return `patch ${movieId} movie`;
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
