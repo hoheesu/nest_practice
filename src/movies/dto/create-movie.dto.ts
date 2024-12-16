7b006f7d9c318d29aa57cd501640e1718cc4d621
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-12-16 16:09:51 +0900

    [#2.4]DTOs and Validation part One

---
+++ src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export default class CreateMovieDto {
+  @IsString()
+  readonly title: string;
+  @IsNumber()
+  readonly year: number;
+  @IsString({ each: true })
+  readonly genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import CreateMovieDto from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -25,23 +26,23 @@ export class MoviesController {
   }
 
   @Get('/:id/')
-  getOne(@Param('id') movieId: string) {
+  getOne(@Param('id') movieId: number) {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     console.log(movieData);
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id/')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id/')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return this.moviesService.update(movieId, updateData);
   }
 }
